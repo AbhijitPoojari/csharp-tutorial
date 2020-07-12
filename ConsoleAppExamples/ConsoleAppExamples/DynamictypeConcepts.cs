ce88368fa3b97d59a7f88924daffd230d239a69e
Abhijit Poojari <49238918+AbhijitPoojari@users.noreply.github.com>
2020-07-12 11:47:45 +0530

    Dynamic Concept added

--- ConsoleAppExamples/ConsoleAppExamples/DynamictypeConcepts.cs
+++ ConsoleAppExamples/ConsoleAppExamples/DynamictypeConcepts.cs
@@ -1,19 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace ConsoleAppExamples
 {
-    class Program
-    {
+    class DynamictypeConcepts
+    { 
         static void Main(string[] args)
         {
-            Console.WriteLine("Var Concept");
-            var i = 100;
+            Console.WriteLine("Dynamic Type Concept");
+            dynamic i = 100;
             Console.WriteLine("i value: {0}, type: {1}", i, i.GetType());
-            var j = "Welcome to Tutlane";
+            dynamic j = "Welcome to Tutlane";
             Console.WriteLine("j value: {0}, type: {1}", j, j.GetType());
-            var k = true;
+            dynamic k = true;
             Console.WriteLine("k value: {0}, type: {1}", k, k.GetType());
-            var l = 20.50;
+            dynamic l = 20.50;
             Console.WriteLine("l value: {0}, type: {1}", l, l.GetType());
             Console.ReadLine();
         }
