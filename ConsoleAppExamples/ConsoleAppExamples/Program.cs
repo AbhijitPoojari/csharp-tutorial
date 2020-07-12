07427463f0993ff79e8298538f357f274c4ee093
Abhijit Poojari <49238918+AbhijitPoojari@users.noreply.github.com>
2020-07-12 11:20:08 +0530

    start project files..

---
+++ ConsoleAppExamples/ConsoleAppExamples/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleAppExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Var Concept");
+            var i = 100;
+
+            Console.WriteLine("i value: {0}, type: {1}", i, i.GetType());
+
+            var j = "Welcome to Tutlane";
+
+            Console.WriteLine("j value: {0}, type: {1}", j, j.GetType());
+
+            var k = true;
+
+            Console.WriteLine("k value: {0}, type: {1}", k, k.GetType());
+
+            var l = 20.50;
+
+            Console.WriteLine("l value: {0}, type: {1}", l, l.GetType());
+
+            Console.ReadLine();
+        }
+    }
+}
